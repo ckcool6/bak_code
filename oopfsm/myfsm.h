a0c41899c4503a0ddf096f39b6f7e0be8f6f2450
Administrator <rzdtjb@gmail.com>
2026-01-12 20:46:26 +0800

    remove doc gen

--- oopfsm/myfsm.h
+++ oopfsm/myfsm.h
@@ -7,18 +7,19 @@
 #include <stdbool.h>
 
 #define _FSM_SEND_MESSAGE_(method, ...) \
-    fsm_##method(__VA_ARGS__)         
-
+  fsm_##method(__VA_ARGS__)
 
 struct FSM;
 
-typedef struct State {
+typedef struct State
+{
   char *name;
   void (*handle)(struct FSM *, int, void **);
   struct State *next;
 } State;
 
-typedef struct FSM {
+typedef struct FSM
+{
   State *fsm_base;
   State *fsm_current_state;
   char *fsm_cur_state_name;
@@ -27,71 +28,41 @@ typedef struct FSM {
 } FSM;
 
 /**
- * @brief 初始化状态机
- * 
- * @param this
- * @return int
+ * 初始化状态机
  */
-int fsm_init(FSM *this);
+int fsm_init(FSM *);
 
 /**
- * @brief 执行下一个状态
- * 
- * @param this
- * @return int
+ * 执行下一个状态
  */
-bool fsm_next_state(FSM *this);
+bool fsm_next_state(FSM *);
 
 /**
- * @brief 添加新状态
- * 
- * @param this
- * @param state
- * @param handle
- * @return int
+ * 添加新状态
  */
-int fsm_add(FSM *this, char *state, void (*handle)(FSM *, int, void **));
+int fsm_add(FSM *, char *, void (*handle)(FSM *, int, void **));
 
 /**
- * @brief 设置默认状态
- * 
- * @param this
- * @param func
- * @return int
+ * 设置默认状态
  */
-int fsm_default(FSM *this, void (*handle)(FSM *, int, void **));
+int fsm_default(FSM *, void (*handle)(FSM *, int, void **));
 
 /**
- * @brief 删除状态
- * 
- * @param this
- * @param state
- * @return int
+ * 删除状态
  */
-bool fsm_remove(FSM *this, char *state);
+bool fsm_remove(FSM *, char *);
 
 /**
- * @brief 切换状态
- * 
- * @param this
- * @param state
- * @param event_id
- * @param event
- * @return int
+ * 切换状态
  */
-bool fsm_transfer_state(FSM *this, char *state, int event_id, void **event);
+bool fsm_transfer_state(FSM *, char *, int, void **);
 
 /**
- * @brief 终止状态机
- * 
- * @param obj
+ * 终止状态机
  */
-void fsm_terminate(FSM *this);
+void fsm_terminate(FSM *);
 
 /**
- * @brief 启动状态机
- * 
- * @param this 
- * @return int 
+ * 启动状态机
  */
-int fsm_start(FSM *this);
+int fsm_start(FSM *);
